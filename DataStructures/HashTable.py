8d5df881134a83ff08eddbf4eaf1ca5998d49f37
Mihir Atrey <mihir.atrey@gmail.com>
2024-11-05 15:58:51 -0500

    add open addressing with linear probing impl to hash table and tests for same

--- DataStructures/HashTable.py
+++ DataStructures/HashTable.py
@@ -3,61 +3,38 @@
 #   are integers drawn from a finite set 1, 2, .., n, and initialization can take O(n) time.
 
 from abc import ABC, abstractmethod
+from shutil import Error
 
+DELETED = object()  # Unique marker for deleted entries
 
 class HashTable(ABC):
-    @classmethod
-    def __new__(cls, collision_res: str = "chaining", init_capacity: int = 31, load_factor: float = 0.7,
-                hash_function: str = "division", *args, **kwargs):
-
-        if collision_res == "chaining":
-            return _ChainingHashTable(init_capacity, load_factor, hash_function)
+    @staticmethod
+    def create(collision_res: str = "open_addressing", **kwargs):
         if collision_res == "open_addressing":
-            return _OpenAddressingHashTable(init_capacity, load_factor, hash_function)
+            return _OpenAddressingHashTable(**kwargs)
+        # elif collision_res == "chaining":
+        #     return _ChainingHashTable(**kwargs)
 
-    @abstractmethod
-    def __init__(self, init_capacity=31, load_factor=0.7, hash_function="division"):
+    def __init__(self, init_capacity: int, hash_function: str):
         self.capacity = init_capacity
-        self.load_factor = load_factor
         self.hash_function = hash_function
         self.size = 0
-        self.table = [None]*init_capacity
+        self.table = [None] * init_capacity
 
-    class Node:
-        def __init__(self, key, val):
-            self.key = key
-            self.val = val
-            self.nxt = None
-
-    @abstractmethod
-    def _hash(self, key) -> int:
-        key_hash = -1
-        # If key is String:
-        # Polynomial Rolling Hash: Treat the string as a polynomial and compute
-        # an integer value using a constant p, e.g., hash_value = sum(ord(char) * p ** i
-        # for i, char in enumerate(string))
-        if isinstance(key, str):
+    def _hash(self, value) -> int:
+        key = -1
+        if isinstance(value, str):
             p = 37
-            key_hash = sum(ord(char) * p **i for i, char in enumerate(key)) % self.capacity
-
-        # If key is Float
-        # A simple approach is to multiply the float by a large power of 10 to remove decimal precision,
-        # converting it to an integer.This works well for hashing as it makes slight changes in the
-        # float representation produce unique hash values.Example: int(float_val * 10000)
-        elif isinstance(key, float):
-            key_str = str(key)
-            decimal_places = len(key_str.split(".")[1])
-            p = 10 ** decimal_places
-            key_hash = (key * p) % self.capacity
-
-        # If key is Integer
-        elif isinstance(key, int):
-            key_hash = key % self.capacity
-
-        return key_hash
+            key = sum(ord(char) * p ** i for i, char in enumerate(value)) % self.capacity
+        elif isinstance(value, float):
+            p = 10 ** len(str(value).split(".")[1])
+            key = (int(value * p)) % self.capacity
+        elif isinstance(value, int):
+            key = value % self.capacity
+        return key
 
     @abstractmethod
-    def insert(self, key, value) -> bool:
+    def insert(self, value) -> bool:
         pass
 
     @abstractmethod
@@ -65,18 +42,20 @@ class HashTable(ABC):
         pass
 
     @abstractmethod
-    def delete(self, key):
+    def delete(self, value):
         pass
 
 
-class _ChainingHashTable(HashTable):
-    def __init__(self, init_capacity, load_factor, hash_function):
-        super().__init__(init_capacity, load_factor, hash_function)
+class _OpenAddressingHashTable(HashTable, ABC):
+    def __init__(self, init_capacity: int = 31, hash_function: str = "division", load_factor: float = 0.7,
+                 probing: str = "linear"):
 
+        if load_factor > 0.9:
+            raise Error("Can't initialize with load factor greater than 0.9")
 
-class _OpenAddressingHashTable(HashTable):
-    def __init__(self, init_capacity, load_factor, hash_function):
-        super().__init__(init_capacity, load_factor, hash_function)
+        super().__init__(init_capacity, hash_function)
+        self.probing = probing
+        self.load_factor = load_factor
 
     def _grow(self):
         self.capacity *= 2
@@ -88,18 +67,61 @@ class _OpenAddressingHashTable(HashTable):
         self.table = new_table
         return
 
-    def insert(self, key, value) -> bool:
-        key_hash = self._hash(key)
+    def insert(self, value) -> bool:
+        if self.size + 1 >= self.load_factor * self.capacity:
+            self._grow()
 
-        while self.table[key_hash] is not None:
-            if self.size >= 0.7 * self.capacity:
-                self._grow()
+        key = self._hash(value)
 
-            if key_hash >= len(self.table):
-                key_hash = 0
-                continue
+        while True:
+            if self.table[key] is None or self.table[key] == DELETED or self.table[key] == value:
+                break
 
-            key_hash += 1
+            if key + 1 == self.capacity:
+                key = 0
+            else:
+                key += 1
 
-        self.table[key_hash] = value
-        return True
+        if self.table[key] is None or self.table[key] == DELETED:
+            self.table[key] = value
+            self.size += 1
+            return True
+        else:
+            raise ValueError("Value already exists in table")
+
+    def find(self, value) -> int:
+        key = self._hash(value)
+
+        if self.table[key] is None:
+            return -1
+        elif self.table[key] == value:
+            return key
+
+        next_key = key + 1
+
+        while next_key != key:
+            if self.table[next_key] is None or self.table[next_key] == value:
+                break
+
+            if next_key + 1 == self.capacity:
+                next_key = 0
+            else:
+                next_key += 1
+
+        if next_key == key or self.table[next_key] is None:
+            return -1
+        else:
+            return next_key
+
+    def delete(self, value):
+        key = self.find(value)
+
+        if key == -1:
+            raise ValueError("The entered value does not exist")
+        else:
+            self.table[key] = DELETED
+            self.size -= 1
+
+            return value
+
+
